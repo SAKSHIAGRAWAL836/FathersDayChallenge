959f9914894ddfd1f223fad22bc56c48e0873c6c
sakshi-agrawal <156072754+SAKSHIAGRAWAL836@users.noreply.github.com>
2025-05-05 16:16:47 +0530

    Update remove_linked_list_elements.py

--- Day6/remove_linked_list_elements.py
+++ Day6/remove_linked_list_elements.py
@@ -1,19 +1,25 @@
 # LeetCode: https://leetcode.com/problems/remove-linked-list-elements/
 # Date: 2025-05-06
-# Approach: Use dummy node to simplify head deletion
+''' Approach: Create a dummy node pointing to the head to handle edge cases (like deleting head).
+  - Traverse the list and remove nodes whose value equals the target.
+  - Reconnect the previous node to skip the matched node.'''
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(0, head)
+        prev = dummy
 
-def removeElements(head: ListNode, val: int) -> ListNode:
-    dummy = ListNode(-1)
-    dummy.next = head
-    curr = dummy
-    while curr.next:
-        if curr.next.val == val:
-            curr.next = curr.next.next
-        else:
-            curr = curr.next
-    return dummy.next
+        while head :
+            if head.val != val:
+                prev.next = head
+                prev = prev.next
+            head =  head.next
+        prev.next = None
+
+        return dummy.next
+        
