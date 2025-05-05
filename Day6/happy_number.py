1df60f970a014f29aa82efb6b256d3f2663e3830
sakshi-agrawal <156072754+SAKSHIAGRAWAL836@users.noreply.github.com>
2025-05-05 16:18:51 +0530

    Update happy_number.py

--- Day6/happy_number.py
+++ Day6/happy_number.py
@@ -1,10 +1,17 @@
 # LeetCode: https://leetcode.com/problems/happy-number/
 # Date: 2025-05-06
-# Approach: Use a set to detect cycles
+# Approach:  - A number is happy if the process of replacing the number by the sum of the squares of its digits eventually leads to 1.
+ # - Use a set to store previously seen numbers to detect loops.
+ #- Keep applying the transformation until the number becomes 1 or enters a loop.
 
-def isHappy(n: int) -> bool:
-    seen = set()
-    while n != 1 and n not in seen:
-        seen.add(n)
-        n = sum(int(c)**2 for c in str(n))
-    return n == 1
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        vis = set()
+        while n != 1 and n not in vis:
+            vis.add(n)
+            x = 0
+            while n:
+                n, v = divmod(n, 10)
+                x += v * v
+            n = x
+        return n == 1
