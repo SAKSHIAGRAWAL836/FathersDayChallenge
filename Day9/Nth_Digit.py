cf7668a416a02436cdbcc6916a6a9fbb98434b8f
sakshi-agrawal <156072754+SAKSHIAGRAWAL836@users.noreply.github.com>
2025-05-08 17:56:04 +0530

    Update Nth_Digit.py

--- Day9/Nth_Digit.py
+++ Day9/Nth_Digit.py
@@ -1,1 +1,14 @@
+# LeetCode: https://leetcode.com/problems/nth-digit/description/
+# Date: 2025-05-08
+# Approach: Dived into a number sequence and pinpointed the nth digit using math and logic. A satisfying brain teaser!
 
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        k, cnt = 1, 9
+        while k * cnt < n:
+            n -= k * cnt
+            k += 1
+            cnt *= 10
+        num = 10 ** (k - 1) + (n - 1) // k
+        idx = (n - 1) % k
+        return int(str(num)[idx])
