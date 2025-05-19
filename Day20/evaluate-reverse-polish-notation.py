0f95d7c4ea5e0861affb954753fc27c8cd378752
sakshi-agrawal <156072754+SAKSHIAGRAWAL836@users.noreply.github.com>
2025-05-19 21:25:38 +0530

    Create evaluate-reverse-polish-notation.py

---
+++ Day20/evaluate-reverse-polish-notation.py
@@ -0,0 +1,18 @@
+# Leetcode: https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+# Date: 2025-05-19
+# Approach: 
+
+def evalRPN(self, tokens: List[str]) -> int:
+        opt = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": operator.truediv,
+        }
+        s = []
+        for token in tokens:
+            if token in opt:
+                s.append(int(opt[token](s.pop(-2), s.pop(-1))))
+            else:
+                s.append(int(token))
+        return s[0]
